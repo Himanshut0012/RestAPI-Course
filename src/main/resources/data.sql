f327ea34d188410290c8f4b5ccaa147051453475
Himanshu Tripathi <himanshut0012@gmail.com>
2022-12-12 18:41:30 +0530

    create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into course values (1,'learning',sysdate(),'python');
+insert into course values (2,'job',sysdate(),'java');
+insert into course values (3,'master',sysdate(),'RestAPI');
+insert into course values (4,'fullstack',sysdate(),'focus');
